ef23ce5c38f4ed91cc821953b087601e13fc0196
Kauã Hilton <kaua.hilton@gmail.com>
2023-12-11 20:15:01 -0300

    Atualização de código
    11/12/2023

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,32 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+const multiplicationTable = document.querySelector(
+    "#multiplication-operations"
+);
 // funçoes
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        console.log(result);
+
+        const template = `<div class="row">
+        <div class="operation">${number} x ${i} = </div>
+        <div class="result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser()
+        
+        const htmlTemplate = parser.parseFromString(template, "text/html")
+
+        const row = htmlTemplate.querySelector(".row")
+
+        multiplicationTable.appendChild(row);
+    }
+};
 
 //Eventos
 
@@ -15,7 +40,7 @@ multiplicationForm.addEventListener("submit", (e) => {
 
     const multiplicatorNumber = +multiplicationInput.value;
 
-    if(!multiplicationNumber || !multiplicatorNumber) return;
+    if (!multiplicationNumber || !multiplicatorNumber) return;
 
     console.log(multiplicationNumber, multiplicatorNumber);
 });
